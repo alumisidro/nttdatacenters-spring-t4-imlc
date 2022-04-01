88b23dc4a587327df4f4c0e18118be0b1538a75e
Isidro <alum.isidro.linares.camarena@ieslosremedios.org>
2022-04-01 14:39:58 +0200

    Eliminar imports no usados

--- src/main/java/com/isidro/ejercicio4/controller/ClienteController.java
+++ src/main/java/com/isidro/ejercicio4/controller/ClienteController.java
@@ -1,26 +1,18 @@
 package com.isidro.ejercicio4.controller;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.util.StringUtils;
 
 import com.isidro.ejercicio4.entity.ClienteEntity;
 import com.isidro.ejercicio4.service.IClienteService;
@@ -31,6 +23,12 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+
+	// Página principal
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/clientes";
+	}
 
 	@GetMapping("/clientes")
 	public String mostrarTodos(Model modelAndView) {
@@ -72,7 +70,7 @@ public class ClienteController {
 	// Eliminar por GET
 	// ¿Habría que cambiarlo por POST? ¿Controlar CSRF? Pues ni idea.
 	@GetMapping(value = "/eliminar/{id}")
-	public String deletePost(@PathVariable Long id) throws Exception {
+	public String eliminar(@PathVariable Long id) throws Exception {
 
 		// ¿Control csrf?
 
@@ -87,12 +85,13 @@ public class ClienteController {
 	@PostMapping("/buscarCliente")
 	public String buscarCliente(Model model, @Param("keyword") String keyword) {
 
-		System.out.println(keyword);
-		
+		// Mostrar por consola lo que está buscando
+		System.out.println("Buscando: " + keyword);
+
 		// Insertar cliente
 		List<ClienteEntity> listaClientes = clienteService.buscarPorNombre(keyword);
 
-		// Añadir a la vista la "variable" clientes con el listado obtenido arriba
+		// Añadir a la vista el listado obtenido arriba
 		model.addAttribute("listaClientes", listaClientes);
 
 		// Redirigir a listado
